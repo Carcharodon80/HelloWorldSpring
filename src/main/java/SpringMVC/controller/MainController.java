82fa6c19d3a5913dca3e0b147ffa34cc4d96527d
Akulov Roman <karna241280@gmail.com>
2018-04-19 02:23:35 +0300

    First commit

---
+++ src/main/java/SpringMVC/controller/MainController.java
@@ -0,0 +1,36 @@
+package SpringMVC.controller;
+
+import SpringMVC.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    //Первый метод на старте приложения (см. параметры аннотации и настройки пути деплоя)
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public ModelAndView main(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("userJSP", new User());
+        modelAndView.setViewName("index");
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/check-user")
+    public ModelAndView checkUser(@ModelAttribute("userJSP")User user){
+        ModelAndView modelAndView = new ModelAndView();
+
+        //имя представления, куда нужно будет перейти
+        modelAndView.setViewName("secondPage");
+
+        //записываем в атрибут userJSP(на странице *.jsp используется объект user)
+        modelAndView.addObject("userJSP", user);
+
+        return modelAndView;
+    }
+
+}
